f5ffa62bb124a4bb76520b47ac74925b0a503887
ihsanannashir <ihsanannashir@gmail.com>
2024-10-24 22:01:14 +0700

    add get players

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,20 @@
 import express, { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
 
 const app: Express = express();
 const port = process.env.REST_PORT;
 
+const prisma = new PrismaClient();
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.get("/one", (req: Request, res: Response) => {
-  res.json({ hello: "world" });
+app.get("/players", async (req: Request, res: Response) => {
+  const players = await prisma.player.findMany();
+
+  res.send(players);
 });
 
 app.listen(port, () => {
